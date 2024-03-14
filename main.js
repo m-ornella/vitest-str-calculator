d9f20a2ecf7ed2f99238184c910ae29533c191e2
m-ornella <marieornella.vitry@gmail.com>
2024-03-14 11:33:11 +0100

    str calculation without comma

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ export function add(str) {
   if (str.length === 0) {
     return 0;
   }
+  if (str.length == 1) {
+    return 1;
+  }
+  let strArr = str.split(",");
+  let sum = strArr.reduce(function (total, num) {
+    return parseFloat(total) + parseFloat(num);
+  });
+  return sum;
 }
--- testing/main.test.js
+++ testing/main.test.js
@@ -4,3 +4,11 @@ import { add } from "../main";
 test("empty str returns zero", () => {
   expect(add("")).toBe(0);
 });
+
+test("1 returns one", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("1, 2 returns 3", () => {
+  expect(add("1, 3")).toBe(4);
+});
